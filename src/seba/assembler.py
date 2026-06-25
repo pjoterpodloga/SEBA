fd15fa36a06c4a80efb16ad55442d22e1b5ca789
pjoterpodloga <piotr.buluk2001@gmail.com>
2026-06-25 14:50:03 +0200

    Added methods for writing simulation files.

--- src/seba/assembler.py
+++ src/seba/assembler.py
@@ -17,13 +17,21 @@ class SebaAssembler:
 
         number_of_corners = self.corners.tnoc
         corner_list = self.corners.generate_corner_list()
-        corner_spice_list = self.corners.generate_spice_corners()
-        corner_spice_definition_list = self.corners.generate_spice_definition_corners()
 
         testbench_list = [copy.deepcopy(self.testbench) for _ in range(number_of_corners)]
         control_list = [copy.deepcopy(self.control) for _ in range(number_of_corners)]
 
-        result_spice_files = []
+        testbench_list = self.__adjust_corner_spice_definitions__(testbench_list)
+        control_list = self.__adjust_corner_write_directive__(control_list)
+        result_spice_files = self.__create_final_spice_files__(testbench_list, control_list)
+
+        self.__write_spice_files__(sim_dir, result_spice_files)
+        self.__write_corner_list__(sim_dir, corner_list)
+        pass
+
+    def __adjust_corner_spice_definitions__(self, testbench_list: list[SebaTestbench]) -> list[SebaTestbench]:
+
+        corner_spice_definition_list = self.corners.generate_spice_definition_corners()
 
         for it_sdl, sdl in enumerate(corner_spice_definition_list):
             for it_sd, sd in enumerate(sdl):
@@ -34,12 +42,22 @@ class SebaAssembler:
                     param_idx = testbench_list[it_sdl].param_index_dict[sd.name]
                     testbench_list[it_sdl].se[param_idx] = sd
 
+        return testbench_list
+
+    def __adjust_corner_write_directive__(self, control_list: list[SebaControl]) -> list[SebaControl]:
+
         for it_c, c in enumerate(control_list):
             for it_se, se in enumerate(c.se):
                 if type(se) == ControlWriteDefinition:
                     control_list[it_c].se[it_se].name = control_list[it_c].se[it_se].name + f"_{it_c}"
+
+        return control_list
+    
+    def __create_final_spice_files__(self, testbench_list: list[SebaTestbench], control_list: list[SebaControl]) -> list[list[str]]:
         
-        for it in range(number_of_corners):
+        result_spice_files = []
+
+        for it in range(len(testbench_list)):
             spice_file = []
 
             tb = testbench_list[it].get_spice_lines()
@@ -50,7 +68,29 @@ class SebaAssembler:
             spice_file = spice_file + cntr + end_of_file
             result_spice_files.append(spice_file)
 
-        pass
+        return result_spice_files
+    
+    def __write_spice_files__(self, sim_dir: str, spice_files: list[list[str]]):
+        
+        spice_file_name = self.config.testbench
 
+        for it_sf, sf in enumerate(spice_files):
+            sfn = f"{spice_file_name}_{it_sf}"
+
+            with open(f"{sim_dir}/{sfn}", 'w') as f:
+                f.write(f"* Title: {self.config.name}")
+                for fc in sf:
+                    f.write(fc)
+                    f.write("\n")
+        
+    def __write_corner_list__(self, sim_dir: str, corners: list[str]):
+        corners_list_file_name = "corners.list"
+
+        clfn = f"{sim_dir}/{corners_list_file_name}"
+
+        with open(clfn, 'w') as f:
+            for fc in corners:
+                f.write(fc)
+                f.write("\n")
 
 
