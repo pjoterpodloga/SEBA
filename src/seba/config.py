d1dd7e586b4d5fe373088c08c22d0eebf054052c
pjoterpodloga <piotr.buluk2001@gmail.com>
2026-06-28 17:39:00 +0200

    Refactored meas field to measure in seba config class.

--- src/seba/config.py
+++ src/seba/config.py
@@ -11,7 +11,7 @@ class SebaConfig:
         self.corners = corners
         self.script = script
         self.plot = plot
-        self.meas = meas
+        self.measure = meas
         self.extraction = extraction
         self.sim_dir = sim_dir
         self.config_dir = config_dir
@@ -22,7 +22,7 @@ class SebaConfig:
         AsyncLogger.debug(f"CONTROL = {self.control}")
         AsyncLogger.debug(f"CORNERS = {self.corners}")
         AsyncLogger.debug(f"SCRIPT = {self.script}")
-        AsyncLogger.debug(f"MEAS = {self.meas}")
+        AsyncLogger.debug(f"MEAS = {self.measure}")
         AsyncLogger.debug(f"PLOT = {self.plot}")
         if self.extraction != None:
             AsyncLogger.debug(f"EXTRACTION = {", ".join(self.extraction)}")
